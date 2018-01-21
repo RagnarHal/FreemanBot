9556c65150b260befc02379e62be69a9d4ff7997
RagnarHal <ragnarmh13@ru.is>
2018-01-21 21:23:10 +0000

    Add hug command

---
+++ src/commands/hug.js
@@ -0,0 +1,16 @@
+const hugResponses = [
+  u => `_Hugs ${u}_`,
+  u => `_Gives ${u} a biiiiiiig hug_`,
+  u => `:heart: _Huggles ${u}_ :heart:`,
+  u => `_Hugs ${u}_ ... O_o Is that something in your pocket?`,
+  u => `BEAR HUG FOR ${u.toUpperCase()}!!! :bear: :hugging:`,
+  u => `No homo, ${u}, but.... _huuuuug_`,
+]
+
+export default (message) => {
+  const hugger = message.author.username;
+  
+  const randomResponseIndex = Math.floor(Math.random() * hugResponses.length);
+  const handler = hugResponses[randomResponseIndex];
+  message.channel.send(handler(hugger));
+}
--- src/commands/index.js
+++ src/commands/index.js
@@ -2,10 +2,12 @@ import ping from './ping';
 import say from './say';
 import roll from './roll';
 import fixtable from './fixtable';
+import hug from './hug';
 
 export default {
   ping,
   say,
   roll,
-  fixtable
+  fixtable,
+  hug
 }
