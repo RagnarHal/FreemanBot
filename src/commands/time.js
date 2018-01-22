ff66391c94043f195057bebcb10269840cb244d6
RagnarHal <ragnarmh13@ru.is>
2018-01-22 21:39:02 +0000

    Handle timezone argument for time command, like GMT and EST, and save on API calls when those are requested

--- src/commands/time.js
+++ src/commands/time.js
@@ -1,4 +1,8 @@
+import moment from 'moment-timezone';
 import { getTimeByAddress } from '../services/timezone';
+
+const dateFormatStr = 'MMM Do YYYY';
+const timeFormatStr = 'HH:mm';
 
 export default async (message, args = []) => {
   if (!args.length) {
@@ -7,11 +11,20 @@ export default async (message, args = []) => {
   }
 
   const query = args.join(' ');
+  const queryIsTimezone = moment.tz.zone(query);
+
+  if (queryIsTimezone) {
+    const mmt = moment();
+    const date = mmt.tz(query).format(dateFormatStr);
+    const time = mmt.tz(query).format(timeFormatStr);
+    message.reply(`It is __${time}__, __${date}__ in the **${query.toUpperCase()}** zone`);
+    return;
+  }
 
   try {
     const res = await getTimeByAddress(query);
-    const date = res.time.format('MMMM Do YYYY');
-    const time = res.time.format('HH:mm');
+    const date = res.time.format(dateFormatStr);
+    const time = res.time.format(timeFormatStr);
 
     message.reply(`It is __${time}__, ${date} in **${res.placeName}**`);
   } catch(err) {
