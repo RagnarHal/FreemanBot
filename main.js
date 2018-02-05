7c1fc20fe0745bb1826d2e82465e7e4798f06568
Ragnar Mikael Halldórsson <ragnarmh13@ru.is>
2018-02-05 22:36:58 +0000

    Switched MongoDB out for Postgres (#5)

--- main.js
+++ main.js
@@ -1,10 +1,16 @@
-import 'dotenv/config';
-import logger from './src/logger';
-import initBot from './src/bot';
+import "dotenv/config";
+import logger from "./src/logger";
+import initBot from "./src/bot";
+import { teardown as dbTeardown } from "./src/services/database";
 
 const bot = initBot();
 
-process.on('SIGINT', () => {
-  logger.info('Caught interrupt signal, destroying bot session and exiting');
-  bot.destroy().then(process.exit);
+process.on("SIGINT", async () => {
+  logger.info(
+    "Caught interrupt signal, destroying bot/database session and exiting"
+  );
+  await bot.destroy();
+  await dbTeardown();
+  // eslint-disable-next-line no-process-exit
+  process.exit(0);
 });
--- src/bot.js
+++ src/bot.js
@@ -1,46 +1,61 @@
 if (!process.env.BOT_TOKEN) {
-  throw new Error('Discord token not found, please include it as a BOT_TOKEN environment variable')
+  throw new Error(
+    "Discord token not found, please include it as a BOT_TOKEN environment variable"
+  );
 }
 
-import Discord from 'discord.js';
-import handlers from './commands';
-import config from './config.json';
-import logger from './logger';
-import { init as dbInit } from './services/database';
+import Discord from "discord.js";
+import handlers from "./commands";
+import config from "./config.json";
+import logger from "./logger";
+import { init as dbInit } from "./services/database";
 
 function handleCommand(message, command, args, client) {
   const handler = handlers[command];
-  typeof handler === 'function'
+  typeof handler === "function"
     ? handler(message, args, client)
-    : message.reply(`I don't know how to handle a ${config.prefix}${command} command...`);
+    : message.reply(
+        `I don't know how to handle a ${config.prefix}${command} command...`
+      );
 }
 
 export default function start() {
   const client = new Discord.Client();
   dbInit();
 
-  client.on('ready', () => {
-    logger.info(`Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`);
+  client.on("ready", () => {
+    logger.info(
+      `Bot has started, with ${client.users.size} users, in ${
+        client.channels.size
+      } channels of ${client.guilds.size} guilds.`
+    );
   });
 
-  client.on('message', message => {
-    if (message.author.bot) { return; }
+  client.on("message", message => {
+    if (message.author.bot) {
+      return;
+    }
 
-    if (message.content === '(╯°□°）╯︵ ┻━┻') {
-      handleCommand(message, 'fixtable');
+    if (message.content === "(╯°□°）╯︵ ┻━┻") {
+      handleCommand(message, "fixtable");
       return;
     }
 
     // Check every message for temperature conversions
-    handleCommand(message, 'temperature');
+    handleCommand(message, "temperature");
 
-    if (message.content.indexOf(config.prefix) !== 0) { return; }
-  
-    const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
+    if (message.content.indexOf(config.prefix) !== 0) {
+      return;
+    }
+
+    const args = message.content
+      .slice(config.prefix.length)
+      .trim()
+      .split(/ +/g);
     const command = args.shift().toLowerCase();
-  
+
     handleCommand(message, command, args, client);
-  
+
     // Quotes
     // Question/Answer (like !whatisbestinlife)
     // Lookup (dictionary)
