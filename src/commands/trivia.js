27996bf8c0f7c39886a2a62c76462da947efe81c
cesar romero <cesar.romero.subs@gmail.com>
2018-02-10 20:12:14 -0800

    added trivia skipping functionality. fixed trivia seed.

--- src/commands/trivia.js
+++ src/commands/trivia.js
@@ -1,4 +1,4 @@
-import { getTriviaScore } from "../services/database";
+import { getTriviaScore, voteTriviaSkip, resetTriviaSkip, getNewTrivia } from "../services/database";
 
 export default async (message, args = []) => {
   if (!args.length) {
@@ -12,10 +12,29 @@ export default async (message, args = []) => {
     if (cmd === "score") {
       const score = await getTriviaScore(message.author.id);
       message.reply("Your' score is " + score + "pts");
+
+    } else if (cmd === "skip") {
+
+      const skip_count = await voteTriviaSkip(message.author.id, message.author.username);
+
+      if (skip_count >= 2) {
+
+        await resetTriviaSkip();
+
+        const new_trivia = await getNewTrivia();
+
+        message.reply("Trivia Skipped!");
+        message.reply(`New Question! #${new_trivia.id}: ${new_trivia.question}`);
+      } else {
+        message.reply("Skip Requested! Need " + (2 - skip_count) + " more to skip question");
+      }
+
+
     } else {
-      message.reply("Try using: score");
+      message.reply("Try using: score, skip");
     }
   } catch (err) {
-    message.reply("I couldn't add that quote for some reason, try again.");
+    message.reply("Congratulations, you broke trivia command!");
+    throw err;
   }
 };
